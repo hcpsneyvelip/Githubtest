b78991b7a5c4ba8f422e921008d38d6a0233f07e
Pranav Neyveli <prachess@gmail.com>
2018-01-28 14:31:03 -0500

    added hello gods

--- Githubtest/ViewController.swift
+++ Githubtest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        print("Hello gods")
         
         let x = 2
         print(x+2)
